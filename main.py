439a7b04753ad6ecc575c9c0e2d7bd9f6c840ec3
Boltasov <alexandr.boltasov@mail.ru>
2023-03-25 18:59:17 +0300

    add get_extension and little fix

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import os.path
+
 import requests
 from pathlib import Path
-
+from os.path import splitext, split
+from urllib.parse import urlparse, unquote
 
 def fetch_spacex_last_launch():
     spacex_url = 'https://api.spacexdata.com/v5/launches/5eb87d47ffd86e000604b38a'
@@ -18,9 +21,19 @@ def fetch_spacex_last_launch():
 
     for photo_number, photo_link in enumerate(links_list):
         dir = 'images'
-        filename = f'spacex_{photo_number}.jpeg'
+        photo_name = unquote(split(urlparse(photo_link).path)[1])
+        filename = f'spacex_{photo_name}'
         download_photo(photo_link, dir, filename)
+
+
+def fetch_nasa_picture_of_a_day():
+    url = ''
+
+
+def get_extension(url):
+    return splitext(url)[1]
 
 
 if __name__ == '__main__':
     fetch_spacex_last_launch()
+    #get_extension('https://apod.nasa.gov/apod/image/2303/_GHR3094-venerelunafirma800.jpg')
