7d7cb3effa71de9293c4154ef28f340ffe713b81
Boltasov <31598564+Boltasov@users.noreply.github.com>
2023-04-01 19:48:47 +0300

    add arguments to permanent_publication.py to set up time from terminal

--- permanent_publication.py
+++ permanent_publication.py
@@ -2,11 +2,26 @@ import time
 import telegram
 import os
 import random
+import argparse
 
 from dotenv import load_dotenv
 
 
+def count_seconds(time_string):
+    time_params = time.strptime(time_string, "%H:%M:%S")
+    seconds = ((time_params.tm_hour * 60) + time_params.tm_min) * 60 + time_params.tm_sec
+    return seconds
+
+
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Программа публикует в телеграм канале фото с заданной периодичностью.\n'
+                    'Еcли период не указан, фото публикуются каждые 4 часа.'
+    )
+    parser.add_argument('--pause', help='Пауза между публикациями в формате HH:MM:SS.', required=False)
+    args = parser.parse_args()
+    pause = args.pause
+
     load_dotenv()
     telegram_bot_token = os.environ['TELEGRAM_BOT_TOKEN']
     telegram_channel_id = os.environ['TELEGRAM_CHANNEL_ID']
@@ -22,9 +37,10 @@ if __name__ == '__main__':
                 bot.send_photo(chat_id=telegram_channel_id, photo=open(photo_path, 'rb'))
 
                 time_string = os.getenv('POSTING_TIME')
-                if time_string:
-                    time_params = time.strptime(time_string, "%H:%M:%S")
-                    sleep_seconds = ((time_params.tm_hour * 60) + time_params.tm_min) * 60 + time_params.tm_sec
+                if pause:
+                    count_seconds(pause)
+                elif time_string:
+                    count_seconds(time_string)
                 else:
                     sleep_seconds = 4 * 60 * 60
 
