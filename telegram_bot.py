d9e1b0cdecb14bca261859a15ce3265cb842680a
Boltasov <31598564+Boltasov@users.noreply.github.com>
2023-04-01 15:50:15 +0300

    publish text to Telegram channel

---
+++ telegram_bot.py
@@ -0,0 +1,13 @@
+import telegram
+import os
+from dotenv import load_dotenv
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    telegram_bot_token = os.environ['TELEGRAM_BOT_TOKEN']
+    telegram_channel_id = os.environ['TELEGRAM_CHANNEL_ID']
+
+    bot = telegram.Bot(token=telegram_bot_token)
+    bot.send_message(chat_id=telegram_channel_id, text="My first words")
+
