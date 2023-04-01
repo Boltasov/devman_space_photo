152cae96f544fc7f5277107a54946c0e393c734e
Boltasov <31598564+Boltasov@users.noreply.github.com>
2023-04-01 18:22:01 +0300

    add is_image check to avoid errors and unnecessary files

--- download_images.py
+++ download_images.py
@@ -1,30 +1,37 @@
 import requests
+import os
 
 from pathlib import Path
-from os import environ
 from dotenv import load_dotenv
 
 
 def download_nasa_image(url, filename):
-    folder = 'images'
-    Path(folder).mkdir(exist_ok=True)
-    load_dotenv()
-    nasa_api_key = environ['NASA_API_KEY']
-    params = {
-        'api_key': nasa_api_key
-    }
-    response = requests.get(url, params)
-    response.raise_for_status()
+    if is_picture(filename):
+        folder = 'images'
+        Path(folder).mkdir(exist_ok=True)
+        load_dotenv()
+        nasa_api_key = os.environ['NASA_API_KEY']
+        params = {
+            'api_key': nasa_api_key
+        }
+        response = requests.get(url, params)
+        response.raise_for_status()
 
-    with open(f'{folder}/{filename}', 'wb') as file:
-        file.write(response.content)
+        with open(f'{folder}/{filename}', 'wb') as file:
+            file.write(response.content)
 
 
 def download_spacex_image(url, filename):
-    folder = 'images'
-    Path(folder).mkdir(exist_ok=True)
-    response = requests.get(url)
-    response.raise_for_status()
+    if is_picture(filename):
+        folder = 'images'
+        Path(folder).mkdir(exist_ok=True)
+        response = requests.get(url)
+        response.raise_for_status()
 
-    with open(f'{folder}/{filename}', 'wb') as file:
-        file.write(response.content)
+        with open(f'{folder}/{filename}', 'wb') as file:
+            file.write(response.content)
+
+
+def is_picture(filename):
+    extension = os.path.splitext(filename)[1]
+    return extension in ['.png', '.jpg', '.gif', '.svg']
