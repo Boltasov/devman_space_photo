fc79aec413a47783052552913398429c3f50c83c
Boltasov <31598564+Boltasov@users.noreply.github.com>
2023-04-01 18:10:32 +0300

    add option to publish one photo in case if path is given

--- telegram_bot.py
+++ telegram_bot.py
@@ -2,11 +2,20 @@ import time
 import telegram
 import os
 import random
+import argparse
 
 from dotenv import load_dotenv
 
 
-if __name__ == '__main__':
+def publish_photo(photo_path):
+    load_dotenv()
+    telegram_bot_token = os.environ['TELEGRAM_BOT_TOKEN']
+    telegram_channel_id = os.environ['TELEGRAM_CHANNEL_ID']
+    bot = telegram.Bot(token=telegram_bot_token)
+    bot.send_photo(chat_id=telegram_channel_id, photo=open(photo_path, 'rb'))
+
+
+def permanent_publication():
     load_dotenv()
     telegram_bot_token = os.environ['TELEGRAM_BOT_TOKEN']
     telegram_channel_id = os.environ['TELEGRAM_CHANNEL_ID']
@@ -16,12 +25,12 @@ if __name__ == '__main__':
         path = 'images'
         folder = os.walk(path)
         print('iteration')
-        for _, _, files in folder:
+        for _, _, files in folder:  # One iteration. Get files list from generator
             random.shuffle(files)
             print(files)
-            for file in files:
-                file_path = f'{path}/{file}'
-                bot.send_photo(chat_id=telegram_channel_id, photo=open(file_path, 'rb'))
+            for photo in files:
+                photo_path = f'{path}/{photo}'
+                bot.send_photo(chat_id=telegram_channel_id, photo=open(photo_path, 'rb'))
 
                 time_string = os.getenv('POSTING_TIME')
                 if time_string:
@@ -31,3 +40,18 @@ if __name__ == '__main__':
                     sleep_seconds = 4*60*60
 
                 time.sleep(sleep_seconds)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Программа публикует в телеграм канале фото, найденное по заданному пути.\n'
+                    'Еcли путь не указан, начинает публикацию всех фото из папки images в этой директории.'
+    )
+    parser.add_argument('--path', help='Укажите путь к фотографии, которую хотите опубликовать.', required=False)
+    args = parser.parse_args()
+    path = args.path
+
+    if path:
+        publish_photo(path)
+    else:
+        permanent_publication()
