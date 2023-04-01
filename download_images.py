afa5de6e5d4582a5e3f804b44631fadf2a31395d
Boltasov <31598564+Boltasov@users.noreply.github.com>
2023-04-01 15:32:04 +0300

    fix logic with .env in download_images.py

--- download_images.py
+++ download_images.py
@@ -2,11 +2,13 @@ import requests
 
 from pathlib import Path
 from os import environ
+from dotenv import load_dotenv
 
 
 def download_nasa_image(url, filename):
     folder = 'images'
     Path(folder).mkdir(exist_ok=True)
+    load_dotenv()
     nasa_api_key = environ['NASA_API_KEY']
     params = {
         'api_key': nasa_api_key
