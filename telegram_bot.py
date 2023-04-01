a48558135b57da89cb795ab87150d21454018579
Boltasov <31598564+Boltasov@users.noreply.github.com>
2023-04-01 17:03:51 +0300

    add delayed publication to telegram bot

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,5 +1,8 @@
+import time
 import telegram
 import os
+import random
+
 from dotenv import load_dotenv
 
 
@@ -7,7 +10,21 @@ if __name__ == '__main__':
     load_dotenv()
     telegram_bot_token = os.environ['TELEGRAM_BOT_TOKEN']
     telegram_channel_id = os.environ['TELEGRAM_CHANNEL_ID']
-
     bot = telegram.Bot(token=telegram_bot_token)
-    bot.send_photo(chat_id=telegram_channel_id, photo=open('images/nasa_elnino4_jpl.jpg', 'rb'))
+
+    while True:
+        path = 'images'
+        folder = os.walk(path)
+        print('iteration')
+        for _, _, files in folder:
+            random.shuffle(files)
+            print(files)
+            for file in files:
+                file_path = f'{path}/{file}'
+                bot.send_photo(chat_id=telegram_channel_id, photo=open(file_path, 'rb'))
+
+                time_string = os.environ['POSTING_TIME']
+                time_params = time.strptime(time_string, "%H:%M:%S")
+                sleep_seconds = ((time_params.tm_hour * 60) + time_params.tm_min) * 60 + time_params.tm_sec
+                time.sleep(sleep_seconds)
 
