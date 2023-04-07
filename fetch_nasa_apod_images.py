793894e42f0fe8a49545ab37f6a6f2a9cdf5d4a3
Boltasov <31598564+Boltasov@users.noreply.github.com>
2023-04-07 17:02:08 +0300

    allow a user to set up count of downloading nasa apod images

--- fetch_nasa_apod_images.py
+++ fetch_nasa_apod_images.py
@@ -1,6 +1,7 @@
 import os.path
 import requests
 import os
+import argparse
 
 from urllib.parse import urlparse, unquote
 from dotenv import load_dotenv
@@ -13,10 +14,10 @@ def get_apod_img_name(img_link):
     return filename
 
 
-def fetch_nasa_apod_images(nasa_params):
+def fetch_nasa_apod_images(nasa_params, photo_count):
     nasa_apod_url = 'https://api.nasa.gov/planetary/apod'
     params = nasa_params
-    params['count'] = 10
+    params['count'] = photo_count
 
     response = requests.get(nasa_apod_url, params)
     response.raise_for_status()
@@ -27,10 +28,17 @@ def fetch_nasa_apod_images(nasa_params):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Программа загружает в папку "/images" фотографии NASA APOD'
+    )
+    parser.add_argument('--count', help='Количество фотографий', required=False, default='10')
+    args = parser.parse_args()
+    photo_count = args.count
+
     load_dotenv()
     nasa_api_key = os.environ['NASA_API_KEY']
     nasa_params = {
         "api_key": nasa_api_key,
     }
-    fetch_nasa_apod_images(nasa_params)
+    fetch_nasa_apod_images(nasa_params, photo_count)
     print(f'Изображения появятся в папке images')
